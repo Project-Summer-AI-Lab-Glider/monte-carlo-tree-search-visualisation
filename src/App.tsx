95d4096b4fa57ff74647cd72fd541b6004d9a297
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-03-28 12:00:40 +0200

    Add lodash and types to project

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { applyMiddleware, compose, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { Provider } from 'react-redux';
 import { appReducer } from './state/appReducer';
+import React, { useState } from 'react';
+import { setSyntheticLeadingComments } from 'typescript';
 
 
 const composedEnhancer = composeWithDevTools(
@@ -16,11 +18,47 @@ export const appStore = createStore(appReducer, composedEnhancer);
 function App() {
   return (
     <Provider store={appStore}>
-      <div>
-        App
-      </div>
+      <Timer
+        date={new Date()} 
+      />
     </Provider>
   );
+
 }
+
+
+type Test = "test" | "not--test";
+
+interface TimerProps {
+  classname?: string;
+  date: Date;
+} 
+
+function classNameFoo(foo: string) {
+
+};
+
+function Timer(props: TimerProps ) {
+  
+
+  const [time, setTime] = useState(0);
+  debugger;
+  const {
+    classname
+  } = props;
+
+  if (!!classname) {
+    classNameFoo(classname);
+  }
+
+  setTimeout(() => {
+    setTime(prev => prev + 1);
+  }, 10000)
+
+  return <div className={classname}>
+    {time}
+  </div>
+}
+
 
 export default App;
