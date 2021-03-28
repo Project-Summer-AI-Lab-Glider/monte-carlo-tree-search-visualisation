df8490889939ba15a08adbcc0ed6f73ac2a75123
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-03-28 10:00:30 +0200

    Application setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, compose, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { Provider } from 'react-redux';
+import { appReducer } from './state/appReducer';
+
+
+const composedEnhancer = composeWithDevTools(
+  compose(applyMiddleware(thunkMiddleware))
+);
+
+export const appStore = createStore(appReducer, composedEnhancer);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={appStore}>
+      <div>
+        App
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/state/appReducer.ts
@@ -0,0 +1,10 @@
+export function appReducer(
+    state: unknown,
+    action: unknown
+  ): unknown {
+      return {
+            'state': 1
+    };
+}
+  
+  
