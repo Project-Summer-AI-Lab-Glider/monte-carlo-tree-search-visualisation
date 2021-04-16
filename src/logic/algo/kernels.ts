6552978a3384495661e6fcaa9920679c33842fca
alicjak1519 <akrzeminska1519@gmail.com>
2021-04-16 08:49:39 +0200

    Add ubcKernel logic

--- src/logic/algo/kernels.ts
+++ src/logic/algo/kernels.ts
@@ -2,4 +2,8 @@ export const ubcKernel = (
   meanNodeValue: number,
   visitsOfNode: number,
   visitsOfParent: number
-): number => 0;
+): number => {
+  var explorationWeight = 0.1;
+  var logVisitsOfParent = Math.log(visitsOfParent);
+  return meanNodeValue + explorationWeight * Math.sqrt(logVisitsOfParent / visitsOfNode);
+};
