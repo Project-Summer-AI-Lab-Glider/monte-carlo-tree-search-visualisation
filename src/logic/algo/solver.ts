afbb1233fe05d95c0be2621a62d7c11cb5322eff
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-04-26 16:50:28 +0200

    Adds algorithm preprocessing before displaying it to user

--- src/logic/algo/solver.ts
+++ src/logic/algo/solver.ts
@@ -1,5 +1,5 @@
-import { ubcKernel } from "./kernels";
 import { TreeNode } from "../treeBuilder/treeNode";
+import { ubcKernel } from "./kernels";
 
 export interface MonteCarloTreeSearchHyperParams {
   kernel?: (meanNodeValue: number, visitsOfNode: number, visitsOfParent: number) => number;
@@ -7,13 +7,20 @@ export interface MonteCarloTreeSearchHyperParams {
   numIterations: number;
 }
 
-export interface AlgorithmRunner {
+export interface Solver {
   run(root: TreeNode, hyperParams: MonteCarloTreeSearchHyperParams): TreeNode;
   choose(node: TreeNode): null;
   select(node: TreeNode): null;
   expand(node: TreeNode): null;
 }
-export class MonteCarloTreeSearch implements AlgorithmRunner {
+
+export type SolverFunction = {
+  [key in keyof Solver]: Solver[key];
+}[keyof Solver];
+
+export type StepToImplementName = keyof Solver;
+
+export class MonteCarloTreeSearch implements Solver {
   private kernel: NonNullable<MonteCarloTreeSearchHyperParams["kernel"]> = ubcKernel;
 
   run(root: TreeNode, hyperParams: MonteCarloTreeSearchHyperParams): TreeNode {
--- src/logic/codePreprocessor/codePreprocessor.ts
+++ src/logic/codePreprocessor/codePreprocessor.ts
@@ -1,31 +1,61 @@
-import * as _ from "lodash";
-import { AlgorithmRunner, MonteCarloTreeSearch } from "../algo/solver";
+import { Opaque } from "../../utlis";
+import { MonteCarloTreeSearch, Solver, SolverFunction, StepToImplementName } from "../algo/solver";
+import { StringifiedFunction } from "./stringifiedFunction";
 
-type StepToImplement = keyof AlgorithmRunner;
-const functionsToImplement: Record<StepToImplement, string> = {
-  choose: "Selects child for which algorithm will make rolout in current step",
-  expand: "Expands selected path",
-  select: "Selects next node",
-  run: "Executes all steps needed to complete an aglo",
+type FunctionDescription = Opaque<"Description", string>;
+const functionsToImplement: Record<StepToImplementName, FunctionDescription> = {
+  choose: "Selects child for which algorithm will make rolout in current step" as FunctionDescription,
+  expand: "Expands selected path" as FunctionDescription,
+  select: "Selects next node" as FunctionDescription,
+  run: "Executes all steps needed to complete an aglo" as FunctionDescription,
 };
 
+const UserDefinedClassName = "AlgorithmRunner";
+const CONSTRUCTOR_FUNC_NAME = "constructor";
 export class AlgorithmCodePreprocessor {
   static getAlgorithmCode(): string {
-    const stepsWithComments = this.replaceImplementations();
-    // TODO
-    return MonteCarloTreeSearch.toString();
+    const codeWithReplacedImplementations = this.replaceImplementations();
+    return codeWithReplacedImplementations;
   }
 
-  static replaceImplementations(): { [key in keyof MonteCarloTreeSearch]: unknown } {
-    return Object.getOwnPropertyNames(MonteCarloTreeSearch.prototype)
-      .filter((key) => key in functionsToImplement)
-      .map((functionName) => {
-        const functionDescription = functionsToImplement[functionName as StepToImplement];
-        const functionImplementation = () => `/**${functionDescription}*/`;
-        return { [functionName]: functionImplementation };
-      })
-      .reduce((acc, dict) => ({ ...acc, ...dict }), {}) as {
-      [key in keyof MonteCarloTreeSearch]: unknown;
-    }; // Fixme
+  static replaceImplementations(): string {
+    const classBody = Object.getOwnPropertyNames(MonteCarloTreeSearch.prototype)
+      .filter((funcName) => funcName !== CONSTRUCTOR_FUNC_NAME)
+      .map((propertyName) => {
+        const originalFunction = this.functionImplementation(propertyName as keyof Solver);
+        const mappedFunction =
+          propertyName in functionsToImplement
+            ? this.createFunctionWithTodo(propertyName as keyof Solver, originalFunction)
+            : new StringifiedFunction(originalFunction.toString());
+        return mappedFunction.format().toString();
+      });
+
+    return `class ${UserDefinedClassName} {
+${classBody.join("\n\n")}
+}`;
+  }
+
+  private static functionImplementation(porperty: keyof Solver): SolverFunction {
+    return MonteCarloTreeSearch.prototype[porperty];
+  }
+
+  private static createFunctionWithTodo(
+    functionName: StepToImplementName,
+    originalFunction: SolverFunction
+  ): StringifiedFunction {
+    const comment = functionsToImplement[functionName];
+    const signature = this.extractSignature(originalFunction);
+    return new StringifiedFunction(`${functionName} (${signature}) {
+      /**
+       * ${comment}
+      */
+    }`);
+  }
+
+  private static extractSignature(func: SolverFunction): string {
+    const funcString = func.toString();
+    const startBraketIndx = funcString.indexOf("(");
+    const endBraketIndx = funcString.indexOf(")");
+    return funcString.substr(startBraketIndx + 1, endBraketIndx - startBraketIndx - 1);
   }
 }
