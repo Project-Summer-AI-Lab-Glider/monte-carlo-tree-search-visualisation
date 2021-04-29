19bcf23ca361864a210437efb16fb191c2b0eb3a
hursliza <45465386+hursliza@users.noreply.github.com>
2021-04-29 19:20:17 +0200

    Added static graph rendered in AlgorithmVisualisationWindow

--- src/components/AlgorithmVisualiationWindow/AlgorithmVisualiationWindowComponent.tsx
+++ src/components/AlgorithmVisualiationWindow/AlgorithmVisualiationWindowComponent.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { AlgorithmVisualiationWindowProps } from "./AlgorithmVisualiationWindowProps";
 import { StyledAlghorithmWindow } from "./styles";
+import { GraphVis } from "../GraphComponent";
+
+// added it here only to pass as argument
+const exampleData = {
+  nodes: [
+    { id: "Node1", x: 350, y: 100 },
+    { id: "Node2", x: 250, y: 220 },
+    { id: "Node3", x: 380, y: 340 },
+    { id: "Node4", x: 450, y: 220 },
+    { id: "Node5", x: 520, y: 340 },
+  ],
+  links: [
+    { source: "Node1", target: "Node2" },
+    { source: "Node1", target: "Node4" },
+    { source: "Node4", target: "Node3" },
+    { source: "Node4", target: "Node5" },
+  ],
+};
 
 function AlgorithmVisualiationWindowF(
   props: AlgorithmVisualiationWindowProps,
@@ -8,7 +26,7 @@ function AlgorithmVisualiationWindowF(
 ): JSX.Element {
   return (
     <StyledAlghorithmWindow {...props} ref={ref}>
-      Algorithm visualiation goes here
+      <GraphVis data={exampleData} />
     </StyledAlghorithmWindow>
   );
 }
