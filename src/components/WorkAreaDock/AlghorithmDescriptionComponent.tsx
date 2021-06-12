39337cbda6acd0305a8584e75ee179c5867a573a
alicjak1519 <akrzeminska1519@gmail.com>
2021-06-13 00:05:33 +0200

    Add some improvements to the theory view

--- src/components/WorkAreaDock/AlghorithmDescriptionComponent.tsx
+++ src/components/WorkAreaDock/AlghorithmDescriptionComponent.tsx
@@ -1,5 +1,26 @@
 import { TheoryContent } from "./TheoryContent";
+import WebFont from 'webfontloader';
+
+WebFont.load({
+   google: {
+     families: ['Scope One:400', 'sans-serif']
+   }
+});
 
 export function AlgorithmDescription(): JSX.Element {
-  return <TheoryContent />;
+  return (
+    <div
+      style={{
+        margin: "15px",
+        padding: "30px",
+        overflowY: "scroll",
+        height: "75%",
+        fontFamily: "Scope One",
+        color: "#36454C",
+        textAlign: "justify"
+      }}
+    >
+      <TheoryContent />
+    </div>)
+
 }
