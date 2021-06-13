1c581e4cdb576cfd417d5d21aea9a3b2c1a8f637
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-06-13 20:00:52 +0200

    Fixes selection step visualisation and description

--- src/components/ActualStepDescription/SelectionDescription.tsx
+++ src/components/ActualStepDescription/SelectionDescription.tsx
@@ -1,16 +1,22 @@
 import { SelectionStepResult } from "../../logic/algo/stepModels";
 import { ComponentWithDescriptionOptions } from "./ComponentWithDescriptionOptions";
 import { RenderTree } from "./RenderTreeHelper";
-import { Title } from "./styled";
+import { Title, Text } from "./styled";
 
 export function SelectionDescription({
   stepResult,
 }: ComponentWithDescriptionOptions<SelectionStepResult>): JSX.Element {
-  const { selectedNodeToVisit } = stepResult;
+  const { selectedNodeToVisit, alreadyVisitedNodes } = stepResult;
   return (
     <>
       <Title>Selection</Title>
       <RenderTree label="Node to visit: " treeRoot={selectedNodeToVisit} />
+      <Text>Already visited nodes</Text>
+      {Array.from(alreadyVisitedNodes.keys()).map((node) => (
+        <Text>
+          {node.id}: {alreadyVisitedNodes.get(node)}
+        </Text>
+      ))}
     </>
   );
 }
