2a1f230ff77d5395e471481da86b659ebeae3fb7
hursliza <45465386+hursliza@users.noreply.github.com>
2021-06-13 22:00:23 +0200

    Added code downloading

--- src/hooks/useTypescriptEditor.tsx
+++ src/hooks/useTypescriptEditor.tsx
@@ -7,12 +7,13 @@ import { ClearFileErrors, SetFileErrors } from "../state/fileErrorsReducer";
 export function useTypescriptEditor(): [
   setEditor: (editor: monacoEditor.editor.IStandaloneCodeEditor) => void,
   compile: () => void,
-  code: string
+  code: string,
+  saveCode: () => void
 ] {
   const [editor, setEditor] = useState<monacoEditor.editor.IStandaloneCodeEditor>();
   const [compiledCode, setCompiledCode] = useState<string>("");
   const dispatch = useDispatch();
-
+  const [notCompiledCode, setNotCompiledCode] = useState<string>("");
   async function getErrorsFromFile(
     compiler: monacoEditor.languages.typescript.TypeScriptWorker,
     fileName: string
@@ -20,6 +21,16 @@ export function useTypescriptEditor(): [
     const semanticDiagnostics = await compiler.getSemanticDiagnostics(fileName);
     const ERROR_CATEGORY = 1; // from api docs
     return semanticDiagnostics.filter((diagnostic) => diagnostic.category === ERROR_CATEGORY);
+  }
+
+  function saveCode() {
+    const codeModel = editor?.getModel();
+    if (codeModel) {
+      const fileWithCode = codeModel.getValue();
+      const FileSaver = require("file-saver");
+      const blob = new Blob([fileWithCode], { type: "text/plain;charset=utf-8" });
+      FileSaver.saveAs(blob, "mcts_code.tsx");
+    }
   }
 
   async function compile() {
@@ -35,6 +46,7 @@ export function useTypescriptEditor(): [
         dispatch(SetFileErrors(errors));
         return;
       }
+      setNotCompiledCode(fileName);
       dispatch(ClearFileErrors());
       const compiledModels = await compiler.getEmitOutput(codeModel.uri.toString());
       const mainCode = compiledModels.outputFiles[0].text;
@@ -43,5 +55,5 @@ export function useTypescriptEditor(): [
       // dispatch(action)
     }
   }
-  return [setEditor, compile, compiledCode];
+  return [setEditor, compile, compiledCode, saveCode];
 }
