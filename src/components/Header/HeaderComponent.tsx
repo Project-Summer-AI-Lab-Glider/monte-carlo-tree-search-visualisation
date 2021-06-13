a5213021f30cc77fab382c034338a94b24ffb474
hursliza <45465386+hursliza@users.noreply.github.com>
2021-06-13 18:42:42 +0200

    Moved "help" button to console toolbar

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 import Logo from "../../assets/app-logo.svg";
 import { StyledHeader, StyledLabel, StyledLogo } from "./styles";
-import { IconButton } from "@material-ui/core";
-import HelpIcon from "@material-ui/icons/Help";
-import { HelpModal } from "./HelpModal";
 
 export function Header(): JSX.Element {
-  const [openModal, setOpen] = useState(false);
-
   return (
     <StyledHeader>
       <StyledLogo src={Logo} alt="logo" />
       <StyledLabel>Monte Carlo How-To</StyledLabel>
-      <IconButton
-        style={{ marginLeft: "auto", marginRight: "8px", color: "lightgray" }}
-        onClick={() => setOpen(true)}
-      >
-        <HelpIcon />
-      </IconButton>
-      <HelpModal open={openModal} onClose={() => setOpen(false)} />
     </StyledHeader>
   );
 }
